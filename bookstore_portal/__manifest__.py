180c237f88e19fd32d60ca1ed5b4c5e57c012528
Nasser <abdounasser202@gmail.com>
2023-05-24 13:49:35 +0200

    Porting to v15

--- bookstore_portal/__manifest__.py
+++ bookstore_portal/__manifest__.py
@@ -3,7 +3,7 @@
     'summary': """Customers can get books throughout the portal""",
     'category': 'Sales',
     "images": ['static/description/images/undraw_books_6rhq.png'],
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
     "author": "Nasser",
     "website": "https://nasser.cm/",
     "license": "LGPL-3",
@@ -15,13 +15,21 @@
         'bookstore_backend',
     ],
     'data': [
-        'views/assets.xml',
         'views/index.xml',
         'views/product_template.xml',
     ],
-    'qweb': [
-        'static/src/components/templates.xml',
-    ],
+    'assets': {
+        'web.assets_frontend': [
+            'bookstore_portal/static/src/css/style.css',
+            'bookstore_portal/static/src/components/list/list.js',
+            'bookstore_portal/static/src/components/search/search.js',
+            'bookstore_portal/static/src/components/app.js',
+            'bookstore_portal/static/src/main.js'
+        ],
+        'web.assets_qweb': [
+            'bookstore_portal/static/src/components/templates.xml',
+        ],
+    },
     'installable': True,
     'application': True,
     'auto_install': False,
